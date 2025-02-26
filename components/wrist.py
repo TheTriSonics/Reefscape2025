6b68705ed2b033de5db5c0b6fb5f9664aca7fff5
Student Coder <studentcoder4003@gmail.com>
2025-02-25 20:14:15 -0500

    It works!

--- components/wrist.py
+++ components/wrist.py
@@ -4,8 +4,9 @@ from phoenix6.hardware import TalonFX, CANcoder
 from magicbot import feedback, tunable
 from phoenix6.controls import (
     MotionMagicVoltage,
+    MotionMagicDutyCycle
 )
-from phoenix6.configs import TalonFXConfiguration
+from phoenix6.configs import TalonFXConfiguration, MotorOutputConfigs
 from phoenix6 import configs, signals
 from utilities.game import ManipLocation
 from ids import TalonId, CancoderId
@@ -16,9 +17,9 @@ pn = wpilib.SmartDashboard.putNumber
 class WristComponent:
     motor = TalonFX(TalonId.MANIP_WRIST.id, TalonId.MANIP_WRIST.bus)
     encoder = CANcoder(CancoderId.MANIP_WRIST.id, CancoderId.MANIP_WRIST.bus)
-    default_pos = 135.0
+    default_pos = 1.0
     target_pos = tunable(default_pos)
-    motor_request = MotionMagicVoltage(0, override_brake_dur_neutral=True)
+    motor_request = MotionMagicDutyCycle(0, override_brake_dur_neutral=True)
     
     def __init__(self):
         enc_config = configs.CANcoderConfiguration()
@@ -33,36 +34,42 @@ class WristComponent:
         config.slot0.k_s = 0.0
         config.slot0.k_v = 0.8
         config.slot0.k_a = 0.01
-        config.slot0.k_p = 0.5
-        config.slot0.k_i = 0
-        config.slot0.k_d = 0.1
-        config.motion_magic.motion_magic_cruise_velocity = 100
-        config.motion_magic.motion_magic_acceleration = 1600
-        config.motion_magic.motion_magic_jerk = 4000
+        config.slot0.k_p = 10.0
+        config.slot0.k_i = 0.0
+        config.slot0.k_d = 0.0
+        config.motion_magic.motion_magic_cruise_velocity = 1
+        config.motion_magic.motion_magic_acceleration = 50
+        config.motion_magic.motion_magic_jerk = 250
         config.feedback.feedback_remote_sensor_id = self.encoder.device_id
         config.feedback.feedback_sensor_source = signals.FeedbackSensorSourceValue.FUSED_CANCODER
-        # The sensor is on the output shaft, so the sensor to mechanism ratio is 1
         config.feedback.sensor_to_mechanism_ratio = 1
-        # The rotor to sensor should be the ratio of the gears connecting the motor
-        # to the wrist pivot.
-        config.feedback.rotor_to_sensor_ratio = 1.0 
-        self.motor.set_position(self.default_pos)
-        self.encoder.set_position(self.default_pos)
+        config.feedback.rotor_to_sensor_ratio = 105
+        config.closed_loop_general.continuous_wrap = True
+        config_output = MotorOutputConfigs()
+        config_output.neutral_mode = signals.NeutralModeValue.BRAKE
         self.motor.configurator.apply(config)  # type: ignore
+        self.motor.configurator.apply(config_output)
     
     @feedback
     def get_position(self) -> float:
-        return self.motor.get_position().value
+        ang = self.motor.get_position().value * 360
+        # normalize to 0-360
+        ang = ang % 360
+        return ang
+
+    @feedback
+    def get_encoder_position(self) -> float:
+        return self.encoder.get_position().value * 360
 
     @feedback
     def at_goal(self):
         current_pos = self.get_position()
+        _ = self.get_encoder_position()
         target_loc = ManipLocation(0, 0, self.target_pos)
         current_loc = ManipLocation(0, 0, current_pos)
         return current_loc == target_loc
 
     def execute(self):
-
-        if not self.at_goal():
-            req = self.motor_request.with_position(self.target_pos)
-            self.motor.set_control(req)
+        can_coder_target = self.target_pos / 360
+        req = self.motor_request.with_position(can_coder_target)
+        self.motor.set_control(req)
