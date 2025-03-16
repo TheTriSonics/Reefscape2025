76772727ae5611da8709f44efd3df6f13a5b2b42
Justin Buist <justin.buist@gmail.com>
2025-03-16 14:30:09 -0400

    Taking 4987's std dev calculation

--- components/vision.py
+++ components/vision.py
@@ -17,15 +17,15 @@ class VisionComponent():
 
     drivetrain: DrivetrainComponent
 
-    std_x = tunable(0.4)
-    std_y = tunable(0.4)
-    std_rot = tunable(0.2)
+    all_cams = tunable(False)
+    center_only = tunable(False)
 
     def __init__(self) -> None:
         self.timer = Timer()
         # Front cameras are backwards in left/right orientation!
         self.camera_fr = PhotonCamera("fl")
         self.camera_fl = PhotonCamera("fr")
+        self.camera_fc = PhotonCamera("fc")
         self.camera_bl = PhotonCamera("bl")
 
         self.camera_fr_offset = Transform3d(
@@ -43,6 +43,15 @@ class VisionComponent():
                 units.inchesToMeters(7.5),
             ),
             Rotation3d.fromDegrees(0, 22.5, -8.0),
+        )
+
+        self.camera_fc_offset = Transform3d(
+            Translation3d(
+                units.inchesToMeters(5.5),  # Forward/backward offset
+                units.inchesToMeters(0.0),
+                units.inchesToMeters(12.5),
+            ),
+            Rotation3d.fromDegrees(0, 22.5, 0.0),
         )
 
         self.camera_bl_offset = Transform3d(
@@ -68,6 +77,12 @@ class VisionComponent():
             self.camera_fl,
             self.camera_fl_offset,
         )
+        self.pose_estimator_fc = PhotonPoseEstimator(
+            field,
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+            self.camera_fc,
+            self.camera_fc_offset,
+        )
         self.pose_estimator_bl = PhotonPoseEstimator(
             field,
             PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
@@ -85,56 +100,77 @@ class VisionComponent():
             .getStructTopic("/components/vision/pose_fl", Pose2d)
             .publish()
         )
+        self.publisher_fc = (
+            ntcore.NetworkTableInstance.getDefault()
+            .getStructTopic("/components/vision/pose_fc", Pose2d)
+            .publish()
+        )
         self.publisher_bl = (
             ntcore.NetworkTableInstance.getDefault()
             .getStructTopic("/components/vision/pose_bl", Pose2d)
             .publish()
         )
 
-        self.cameras = [self.camera_fr, self.camera_fl, self.camera_bl]
+        self.cameras = [self.camera_fr, self.camera_fl, self.camera_fc, self.camera_bl]
         self.pose_estimators = [
             self.pose_estimator_fr,
             self.pose_estimator_fl,
+            self.pose_estimator_fc,
             self.pose_estimator_bl,
         ]
-        self.publishers = [self.publisher_fr, self.publisher_fl, self.publisher_bl]
+        self.publishers = [
+            self.publisher_fr,
+            self.publisher_fl,
+            self.publisher_fc,
+            self.publisher_bl,
+        ]
 
     def execute(self) -> None:
         from math import radians
         setDevs = self.drivetrain.estimator.setVisionMeasurementStdDevs
         tag_id, tag_dist = Waypoints.closest_reef_tag_id(self.drivetrain.get_pose())
-        for cam, pose_est, pub in zip(
-            self.cameras, self.pose_estimators, self.publishers
-        ):
+        if tag_dist < 1.0:
+            # Only use the center camera
+            z = zip([self.camera_fc], [self.pose_estimator_fc], [self.publisher_fc])
+            self.center_only = True
+            self.all_cams = False
+        else:
+            # Use all cameras
+            z = zip(
+                self.cameras, self.pose_estimators, self.publishers
+            )
+            self.center_only = False
+            self.all_cams = True
+
+        for cam, pose_est, pub in z:
             results = cam.getAllUnreadResults()
             for res in results:
                 best_target = res.getBestTarget()
-                if best_target and (best_target.poseAmbiguity > 0.2):
+                if best_target and (best_target.poseAmbiguity > 0.3):
                     # Skip using this pose in a vision update; it is too ambiguous
-                    continue
+                    # continue
+                    pass
+                linear_baseline_std = 0.02  # meters
+                angular_baseline_std = 0.06  # radians
 
-                taget_ids_in_frame = [t.fiducialId for t in res.getTargets()]
-                if tag_dist < 0.5 and tag_id in taget_ids_in_frame:
-                    self.std_x, self.std_y, self.std_rot = 0.01, 0.01, radians(5.0)
-                elif tag_dist < 1.0 and tag_id in taget_ids_in_frame:
-                    self.std_x, self.std_y, self.std_rot = 0.05, 0.05, radians(10)
-                elif tag_dist < 2.0 and tag_id in taget_ids_in_frame:
-                    self.std_x, self.std_y, self.std_rot = 0.1, 0.1, radians(22.5)
-                else:
-                    self.std_x, self.std_y, self.std_rot = 0.4, 0.4, radians(22.5)
-
-                setDevs((self.std_x, self.std_y, self.std_rot))
                 pupdate = pose_est.update(res)
                 if pupdate:
-                    pose = pupdate.estimatedPose
-                    twod_pose = Pose2d(pose.x, pose.y,
-                                        pose.rotation().toRotation2d())
-                    pub.set(twod_pose)
+                    twod_pose = pupdate.estimatedPose.toPose2d()
                     ts = self.timer.getTimestamp() - res.getLatencyMillis() / 1000.0
                     # Check if we're too far off for this to be valid
                     robot_pose = self.drivetrain.get_pose()
-                    xdiff = abs(robot_pose.X() -twod_pose.X())
-                    ydiff = abs(robot_pose.Y() -twod_pose.Y())
-                    distance = math.sqrt(xdiff**2 + ydiff**2) 
-                    if distance < 1 or is_disabled():
+                    pub.set(twod_pose)
+                    dist = robot_pose.relativeTo(twod_pose).translation().norm()
+                    # Reject poses that are more than 1 meter from current
+                    if dist < 1.0 or is_disabled():
+                        # Ok let's figure out a stddev for this
+                        total_dist = 0.0
+                        tag_count = len(res.getTargets())
+                        for t in res.getTargets():
+                            total_dist += t.getBestCameraToTarget().translation().norm()
+                        avg_dist = total_dist / tag_count
+                        std_factor = (avg_dist**2) / tag_count
+                        std_xy = linear_baseline_std * std_factor
+                        std_rot = angular_baseline_std * std_factor
+                        setDevs((std_xy, std_xy, std_rot))
                         self.drivetrain.estimator.addVisionMeasurement(twod_pose, ts)
