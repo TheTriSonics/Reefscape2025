9cdcd85fb8089c92d6794f30f37e0607564b2dd8
Justin Buist <justin.buist@gmail.com>
2025-03-26 15:21:26 -0400

    mypy typing fix

--- components/drivetrain.py
+++ components/drivetrain.py
@@ -230,13 +230,13 @@ class DrivetrainComponent:
         self.snap_heading: float | None = None
 
         # Leaving the old values here, using some more docile ones for driver practice temporarily
-        self.default_xy_pid = (10, 1, 0)
-        self.aggressive_xy_pid = (20, 0, 0)
-        self.heading_pid = (15, 0, 0)
+        self.default_xy_pid = (10.0, 1.0, 0.0)
+        self.aggressive_xy_pid = (20.0, 0.0, 0.0)
+        self.heading_pid = (15.0, 0.0, 0.0)
         if is_sim():
             self.default_xy_pid = (14, 2, 0)
             self.aggressive_xy_pid = (30, 3, 0)
-            self.heading_pid = (15, 0, 0.1)
+            self.heading_pid = (15.0, 0.0, 0.1)
 
         self.choreo_x_controller = PIDController(*self.default_xy_pid)
         self.choreo_y_controller = PIDController(*self.default_xy_pid)
