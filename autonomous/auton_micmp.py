7170485e3967ed522bd0fcd2401fe11722754555
Justin Buist <justin.buist@gmail.com>
2025-03-27 19:37:06 -0400

    Now with a right and a middle routine
    We're looking like a real robot team here!

--- autonomous/auton_micmp.py
+++ autonomous/auton_micmp.py
@@ -18,6 +18,7 @@ from components.gyro import GyroComponent
 from components.battery_monitor import BatteryMonitorComponent
 from components.photoeye import PhotoEyeComponent
 from components.arm import ArmComponent
+from components.wrist import WristComponent
 from autonomous.base import AutonBase
 
 pb = SmartDashboard.putBoolean
@@ -45,7 +46,7 @@ class LeftCoral(AutonBase):
     at_pose_counter = tunable(0)
 
     MODE_NAME = 'MICMP - Left Coral'
-    DEFAULT = True
+    DEFAULT = False
     
     def __init__(self):
         self.coral_scored = 0
@@ -108,13 +109,16 @@ class LeftCoral(AutonBase):
 
     @state(must_finish=True)
     def to_ps(self, state_tm, initial_call):
+        curr_pose = self.drivetrain.get_pose()
         if initial_call:
             # If the elevator is up, we need to bring it down so just reset it
             # We don't just force it home because a previous state might have
             # it in intake already.
             if self.elevator.get_position() > 20:
                 self.manipulator.go_home()
-            self.intimidator.go_drive_swoop(Positions.PS_CLOSEST)
+
+            self.intimidator.prep_pp_trajectory(curr_pose, Positions.PS_CLOSEST, max_vel=2.0, max_accel=1.5)
+            self.intimidator.next_state_now(self.intimidator.follow_pp.name)
         if self.photoeye.coral_held:
             self.next_state_now(self.to_face)
 
@@ -274,3 +278,100 @@ class RightCoral(AutonBase):
             self.manipulator.go_coral_intake()
             if self.elevator.get_position() <= 20:
                 self.next_state_now(self.to_ps)
+
+
+class MiddleDunk(AutonBase):
+    elevator: ElevatorComponent
+    drivetrain: DrivetrainComponent
+    gyro: GyroComponent
+    battery_monitor: BatteryMonitorComponent
+    manipulator: Manipulator
+    wrist: WristComponent
+    arm: ArmComponent
+    intimidator: Intimidator
+    intake_control: IntakeControl
+    photoeye: PhotoEyeComponent
+
+    first_face = tunable('F')
+    fill_face = tunable('E')
+    curr_level = tunable(4)
+    curr_left = tunable(True)
+
+    at_pose_counter = tunable(0)
+
+    MODE_NAME = 'MICMP - Middle Dunk'
+    DEFAULT = True
+    
+    def __init__(self):
+        self.coral_scored = 0
+        self.backup_pose_pub = (
+            ntcore.NetworkTableInstance.getDefault()
+            .getStructTopic("/components/auton_wmi/backup_pose", Pose2d)
+            .publish()
+        )
+
+    def get_initial_pose(self):
+        self.photoeye.back_photoeye = True
+        self.photoeye.coral_held = True
+        return Positions.AUTON_LINE_MID
+
+
+    @state(must_finish=True, first=True)
+    def to_reef(self, initial_call, state_tm):
+        target_pose = Positions.REEF_A_LEFT_CLOSE
+        if initial_call:
+            self.manipulator.set_coral_level(4)
+            self.manipulator.coral_mode()
+            self.arm.target_pos = 90
+            self.intimidator.go_drive_swoop(target_pose)
+        if self.arm.get_position() > 80:
+            self.manipulator.go_coral_prepare_score()
+        if self.at_pose(target_pose) and self.manipulator.at_position():
+            self.intake_control.go_coral_score()
+        if self.photoeye.coral_held is False:
+            self.coral_scored += 1
+            self.next_state_now(self.to_backup)
+
+    @state(must_finish=True)
+    def to_backup(self, state_tm, initial_call):
+        if initial_call:
+            reef_pose = Positions.REEF_A_LEFT_CLOSE
+            backup_pose = reef_pose.transformBy(Transform2d(-1.0, 0, Rotation2d(0)))
+            curr_pose = self.drivetrain.get_pose()
+            self.backup_pose_pub.set(backup_pose)   
+            self.intimidator.prep_pp_trajectory(curr_pose, backup_pose)
+            self.intimidator.next_state_now(self.intimidator.follow_pp)
+
+        if self.manipulator.reef_dist() > self.manipulator.reef_protection_dist:
+            self.manipulator.set_algae_level1()
+            self.manipulator.go_algae_prepare_intake()
+            self.next_state_now(self.get_algae)
+
+    @state(must_finish=True)
+    def get_algae(self, initial_call, state_tm):
+        if initial_call:
+            self.intimidator.go_drive_swoop(Positions.REEF_A)
+        if self.photoeye.algae_held is True:
+            self.manipulator.set_algae_barge()
+            self.manipulator.go_algae_prepare_score()
+            self.next_state_now(self.score_in_barge)
+    
+    @state(must_finish=True)
+    def score_in_barge(self, initial_call, state_tm):
+        target_pose = Positions.BARGE_CENTER
+        if initial_call:
+            self.intimidator.go_drive_swoop(target_pose)
+        if self.at_pose(target_pose) and self.manipulator.at_position():
+            self.intake_control.go_algae_score()
+        if self.photoeye.algae_held is False:
+            self.next_state_now(self.to_away_from_startline)
+
+    @state(must_finish=True)
+    def to_away_from_startline(self, initial_call, state_tm):
+        target_pose = Positions.REEF_F.transformBy(Transform2d(-2.0, 0, Rotation2d(0)))
+        if initial_call:
+            self.wrist.target_pos = ManipLocations.INTAKE_CORAL.wrist_pos
+            self.intimidator.go_drive_swoop(target_pose)
+        if state_tm > 0.5:
+            self.manipulator.go_home()
+
